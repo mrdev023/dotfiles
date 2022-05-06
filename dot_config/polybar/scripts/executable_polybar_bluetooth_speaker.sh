f6fb8f59c05bea796d9ae8880d622dffbd3797da
Florian RICHER <florian.richer.97@outlook.com>
2022-05-06 23:12:06 +0200

    Update dot_config/polybar/scripts/executable_polybar_bluetooth_speaker.sh Update run_once_prepare_arch.sh.tmpl

--- dot_config/polybar/scripts/executable_polybar_bluetooth_speaker.sh
+++ dot_config/polybar/scripts/executable_polybar_bluetooth_speaker.sh
@@ -10,7 +10,8 @@
 source colors.sh
 
 bluetooth_speaker() {
-	SPEAKER_CONNECTED=$(bt-device -i 78:44:05:F6:E9:15 | grep Connected | xargs | cut -d ' ' -f 2)
+	BT_DEVICE="1C:E6:1D:EF:0A:76"
+	SPEAKER_CONNECTED=$(bt-device -i $BT_DEVICE | grep Connected | xargs | cut -d ' ' -f 2)
 	if [ "$SPEAKER_CONNECTED" -eq 1 ]; then
 		speaker_indicator="${faded_green}${RESET}"
 	else
